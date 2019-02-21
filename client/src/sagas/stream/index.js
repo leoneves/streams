80e1d65d0876aa89b4e808bd6c8148f0f78e71c6
leoneves <eleonardoneves@gmail.com>
2019-02-20 23:48:02 -0300

    remove errors: A space is required after '{' objects

--- client/src/sagas/stream/index.js
+++ client/src/sagas/stream/index.js
@@ -9,10 +9,10 @@ import {
 function* fetchStreams() {
   try {
     const streams = yield call(getStreams)
-    yield put({type: FETCH_STREAMS_SUCCEEDED, streams: streams})
+    yield put({ type: FETCH_STREAMS_SUCCEEDED, streams: streams })
   } catch (e) {
-    yield put({type: FETCH_STREAMS_FAILED })
-    yield put({type: FETCH_STREAMS_FAILED_MESSAGE, message: e.message})
+    yield put({ type: FETCH_STREAMS_FAILED })
+    yield put({ type: FETCH_STREAMS_FAILED_MESSAGE, message: e.message })
   }
 }
 
